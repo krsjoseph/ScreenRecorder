c50ff610ca86a360e8a2b075593b797ab749fed7
Krs Joseph <krspjoseph@gmail.com>
2017-01-06 12:31:44 -0400

    electron react app

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './app.global.css';
+import HomePage from './containers/HomePage';
+
+render(
+  <HomePage />,
+  document.getElementById('root')
+);
